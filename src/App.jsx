6718de3f5e587addd923296dd04a4ac49c741151
KMSDINESH <dineshkumar54466@gmail.com>
2025-07-10 20:37:05 +0100

    updated syory

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './Components/Hero'
 import About from './Components/About'
 import Navbar from './Components/Navbar'
 import Features from './Components/Features'
+import Story from './Components/Story'
 
 const App = () => {
   return (
@@ -14,6 +15,8 @@ const App = () => {
       <About />
 
       <Features />
+
+      <Story />
     </main>
   )
 }
